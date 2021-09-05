3b67d45921670308c9dec360ef99b0831778a59d
Andi Nur Inayah <86001320+andinurinayah@users.noreply.github.com>
2021-09-05 14:14:00 +0800

    Update credit_scoring.R

--- credit_scoring.R
+++ credit_scoring.R
@@ -2,14 +2,9 @@ library("openxlsx")
 library("C50")
 
 #Mempersiapkan data
-dataCreditRating <- read.xlsx(xlsxFile = "E://DQLab/R/Finance/credit_scoring_dqlab.xlsx")
+dataCreditRating <- read.xlsx(xlsxFile = "https://storage.googleapis.com/dqlab-dataset/credit_scoring_dqlab.xlsx")
 
 #Mempersiapkan class dan input variables 
-dataCreditRating$risk_rating[dataCreditRating$risk_rating == "1"] <- "satu"
-dataCreditRating$risk_rating[dataCreditRating$risk_rating == "2"]  <-  "dua"
-dataCreditRating$risk_rating[dataCreditRating$risk_rating == "3"]  <-  "tiga"
-dataCreditRating$risk_rating[dataCreditRating$risk_rating == "4"]  <-  "empat"
-dataCreditRating$risk_rating[dataCreditRating$risk_rating == "5"]  <-  "lima"
 dataCreditRating$risk_rating <- as.factor(dataCreditRating$risk_rating) 
 input_columns <- c("durasi_pinjaman_bulan", "jumlah_tanggungan")
 datafeed <- dataCreditRating[ , input_columns ]
@@ -23,23 +18,21 @@ input_training_set <- datafeed[indeks_training_set,]
 class_training_set <- dataCreditRating[indeks_training_set,]$risk_rating
 input_testing_set <- datafeed[-indeks_training_set,]
 
-#menghasilkan model
-risk_rating_model <- C5.0(input_training_set, class_training_set, 
-                          control = C5.0Control(label="Risk Rating"))
-
+#Model
+risk_rating_model <- C5.0(input_training_set, class_training_set, control = C5.0Control(label="Risk Rating"))
 summary(risk_rating_model)
 
-#menghasilkan plot dari model C5.0
+#Diagram desicion tree
 plot(risk_rating_model)
 
-#menggunakan model untuk prediksi testing set
+#Menggunakan model untuk prediksi testing set
 predict(risk_rating_model, input_testing_set)
 
-#menyimpan risk_rating dari data awal dan hasil prediksi testing set ke dalam kolom hasil_prediksi
+#Menyimpan risk_rating dari data awal dan hasil prediksi testing set ke dalam kolom hasil_prediksi
 input_testing_set$risk_rating <- dataCreditRating[-indeks_training_set,]$risk_rating
 input_testing_set$hasil_prediksi <- predict(risk_rating_model, input_testing_set)
 
-#membuat confusion matrix
+#Membuat confusion matrix
 library(reshape2)
 dcast(hasil_prediksi ~ risk_rating, data=input_testing_set)
 
@@ -54,6 +47,5 @@ nrow(input_testing_set[input_testing_set$risk_rating!=input_testing_set$hasil_pr
 #Membuat data frame aplikasi baru
 aplikasi_baru <- data.frame(jumlah_tanggungan = 6, durasi_pinjaman_bulan = 12)
 
-#melakukan prediksi
+#Melakukan prediksi
 predict(risk_rating_model, aplikasi_baru)
-
